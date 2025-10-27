9cb1dada12a582b3f20f9f993aad1443bf9cf35c
Alexander-Rellini <alessandrorellini17@gmail.com>
2025-10-27 11:24:11 +0100

    Update Atleta.java

--- src/Atleta.java
+++ src/Atleta.java
@@ -1,26 +1,51 @@
-import java.util.Scanner;
-public class Atleta implements Runnable{
-    int numero;
-    String nome = "Alexander";
-    double tempo = 0;
-    double metri = 0;
-    final double LUNGHEZZAGARA = 400;
+import java.util.Random;
 
-    public Atleta run(int pNumero, String pNome) {
-        numero= pNumero;
-        nome= pNome;
+
+
+public class Atleta implements Runnable {
+
+    int numero;
+
+    String nome;
+
+    double tempo = 0;
+
+    double metri = 0;
+
+    final double LUNGHEZZAGARA = 50;
+
+
+
+    public Atleta(int pNumero, String pNome) {
+
+        numero = pNumero;
+
+        nome = pNome;
 
     }
+
 
 
     @Override
-    public void run(){
-        while(metri<LUNGHEZZAGARA) {
-        metri+= metripercorsi.nextdouble(bound:10);
-        System.out.println("metri percorsi: ", metri);
 
-            metri=metri+10;
-        tempo=tempo+1;
+    public void run() {
+
+        Random metriPercorsi = new Random();
+
+        while(metri <= LUNGHEZZAGARA) {
+
+            metri += metriPercorsi.nextDouble(10);
+
+            System.out.println(nome + " Metri Percorsi: " + metri);
+
+            try { Thread.currentThread().sleep(1000); }
+
+            catch (InterruptedException e) { System.err.println("Errore sleep"); }
+
         }
+
+        System.out.println(nome + " è al Traguardo");
+
     }
+
 }
