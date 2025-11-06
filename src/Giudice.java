a7bbb18d5ca5e6f47f57e1643c7833dee1d49243
Alexander-Rellini <alessandrorellini17@gmail.com>
2025-11-06 13:13:39 +0100

    Create Giudice.java

---
+++ src/Giudice.java
@@ -0,0 +1,19 @@
+public class Giudice {
+    String Nome;
+    String NomeGara;
+
+
+
+    public void InizioGara(){
+
+    }
+
+    public void Vincitore(){
+
+    }
+
+    public void Podio(){
+
+    }
+
+}
