e683e250b7d173125394e315d603e6b0318f6d37
Alexander-Rellini <alessandrorellini17@gmail.com>
2025-11-18 14:43:52 +0100

    m

--- src/Gara.java
+++ src/Gara.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Gara {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         int MIN_LUNGH = 15;
         int MIN_PART = 2;
         int MAX_PART = 4;
